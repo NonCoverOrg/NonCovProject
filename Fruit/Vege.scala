8d95b4f4974287b51c8222f92a3ed33b3560afd4
olgamaluhacodacy <olga.maluha@codacy.com>
2023-04-13 15:25:04 +0200

    2 commit

---
+++ Fruit/Vege.scala
@@ -0,0 +1,20 @@
+class Vege {
+  def nameVeg(name: String): Unit = {
+    2 + 2
+    println("Hi, my name is " + name)
+    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+//    println("Hi, my name is " + name)
+  }
+}
